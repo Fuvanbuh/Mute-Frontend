8afc144a2af92078494852a8026a70130924f522
juliacanas <julia_cl90@homtail.com>
2019-11-18 10:37:13 +0100

    form story added

--- src/pages/NewStory.js
+++ src/pages/NewStory.js
@@ -1,10 +1,103 @@
 import React, { Component } from 'react'
+import storyService from '../services/story-service'
+
+import FormStory from '../components/FormStory'
 
 class NewStory extends Component {
+  state = {
+    title: "",
+    text: "",
+    question: "",
+    answer1: "",
+    answer2: "",
+    answer3: "",
+    correct: "",
+    creator: "",
+    background: "",
+  }
+
+  handleOnChange = event => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const {
+      title,
+      text,
+      question,
+      answer1,
+      answer2,
+      answer3,
+      correct,
+      creator,
+      background,
+    } = this.state;
+
+
+    const newStory = {
+      title,
+      text,
+      question,
+      answer1,
+      answer2,
+      answer3,
+      correct,
+      background,
+    }
+    /*  const storyCreator={
+       creator
+     }
+ ​
+     const creatorOfStory = authService.me(storyCreator);
+     this.setState({
+       creator:creatorOfStory
+     }) */
+
+    storyService
+      .createStory(newStory)
+      .then(response => {
+        this.props.history.goBack();
+      })
+      .catch(error => console.log(error));
+  };
+
   render() {
+    const {
+      title,
+      text,
+      question,
+      answer1,
+      answer2,
+      answer3,
+      correct,
+      creator,
+      background,
+    } = this.state
+
     return (
       <div>
         <h1>New Story</h1>
+        <FormStory
+          handleOnChange={this.handleOnChange}
+          handleSubmit={this.handleSubmit}
+          title={title}
+          text={text}
+          question={question}
+          answer1={answer1}
+          answer2={answer2}
+          answer3={answer3}
+          correct={correct}
+          creator={creator}
+          background={background}
+
+
+        />
+
       </div>
     )
   }
--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,42 +1,45 @@
-// import React from 'react'
-// // import mapService from '../services/map-service'
-
-// const Questions = (props) => {
-//   console.log(props)
-//   const idMap = props.match.params.idMap
-
-//   return (
-//     <div>
-//       <h1>questions</h1>
-//       {/* <p>{map.story.paragraph[pathNum].text}</p> */}
-//     </div>
-//   )
-// }
-
-// export default Questions
-
 import mapService from '../services/map-service'
 import React, { Component } from 'react'
 
 class Questions extends Component {
   state={
     map: null,
-    selectedOption: "option1"
+    selectedOption: ""
   }
 
   componentDidMount = async () => {
     const { idMap } = this.props.match.params
-
     const oneMap = await mapService.getTheMap(idMap);
     this.setState({
       map: oneMap
     })
+    
   }
+
+  handleFormSubmit  = async (event) => {
+    event.preventDefault()
+    const { idMap } = this.props.match.params
+    const correct = this.state.map.story.paragraph[this.props.match.params.pathNum].correct
+    if(this.state.selectedOption === correct){
+      await mapService.modifyPathMap(idMap)
+    }
+    console.log(this.state.map)
+  }
+
   
+
+  handleOptionChange = (event) => {    
+    console.log(event.target.value)
+    this.setState ({
+      selectedOption: event.target.value
+    })
+  }
+
+
   render() {
     const {pathNum} = this.props.match.params
     const {map} = this.state
-    console.log(map)
+   
     return (
       <div>
         <h1>questions</h1>
@@ -45,22 +48,22 @@ class Questions extends Component {
           <p>{map.story.paragraph[pathNum].question}</p>
           <form onSubmit={this.handleFormSubmit}>
             <label>
-              <input type="radio" value="option1"
-                checked={this.state.selectedOption === 'option1'}
-                onChange={this.handleOptionChange} />{map.story.paragraph[pathNum].answer1}</label>
+              <input type="radio" value="answer1"
+                checked={this.state.selectedOption === 'answer1'}
+                onChange={this.handleOptionChange}/>{map.story.paragraph[pathNum].answer1}</label>
 
             <label>
-              <input type="radio" value="option2"
-                checked={this.state.selectedOption === 'option2'}
-                onChange={this.handleOptionChange} /> {map.story.paragraph[pathNum].answer2}</label>
+              <input type="radio" value="answer2"
+                checked={this.state.selectedOption === 'answer2'}
+                onChange={this.handleOptionChange}/> {map.story.paragraph[pathNum].answer2}</label>
 
             <label>
-              <input type="radio" value="option3"
-                checked={this.state.selectedOption === 'option3'}
-                onChange={this.handleOptionChange} /> {map.story.paragraph[pathNum].answer3} </label>
+              <input type="radio" value="answer3"
+                checked={this.state.selectedOption === 'answer3'}
+                onChange={this.handleOptionChange}/> {map.story.paragraph[pathNum].answer3} </label>
 
             <button type="submit">Enviar</button>
-          </form>
+          </form>         
         </div>
           
         }
